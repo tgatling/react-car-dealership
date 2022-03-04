a680d2125c783d69761ee3f35465c3d5ac4d53bc
tgatling <tequesha.l.gatling@gmail.com>
2022-03-03 21:36:49 -0500

    added components needed for viewing individual cars

---
+++ src/components/car/CarDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CarDetails = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CarDetails;
---
+++ src/pages/ViewCar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import CarDetails from '../components/car/CarDetails';
+
+const ViewCar = () => {
+    return (
+        <CarDetails/>
+    );
+};
+
+export default ViewCar;
