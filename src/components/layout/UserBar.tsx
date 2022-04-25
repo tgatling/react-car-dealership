e742118babe7291250390c5d0e65eb0dfaf93fbd
tgatling <tequesha.l.gatling@gmail.com>
2022-04-25 11:05:21 -0400

    add settings icon to user bar

--- src/components/layout/UserBar.tsx
+++ src/components/layout/UserBar.tsx
@@ -7,9 +7,11 @@ import {
   PAYMENT_HISTORY,
   VIEW_YOUR_CARS,
   MESSAGES,
+  SETTINGS,
 } from '../../models/constants';
 import email from '../../images/email.png';
 import tealEmail from '../../images/teal-email.png';
+import settings from '../../images/settings.png';
 import styles from './UserBar.module.css';
 
 interface userProps {
@@ -43,7 +45,7 @@ const UserBar = ({ username, userId }: userProps) => {
               Offer History
             </NavLink>
           </li>
-          <li className={styles.email}>
+          <li className={styles.icon}>
             <NavLink to={MESSAGES}>
               {unreadCount > 0 ? (
                 <img src={tealEmail} alt='' />
@@ -51,6 +53,11 @@ const UserBar = ({ username, userId }: userProps) => {
                 <img src={email} alt='' />
               )}
               {/* <p>{unreadCount}</p> */}
+            </NavLink>
+          </li>
+          <li className={styles.icon}>
+            <NavLink to={SETTINGS}>
+              <img src={settings} alt='' />
             </NavLink>
           </li>
         </ul>
