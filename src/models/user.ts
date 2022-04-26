9cde2977e5788f08f3a943cf7f175e620ee63300
tgatling <tequesha.l.gatling@gmail.com>
2022-04-25 22:36:59 -0400

    added attributes to user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -3,7 +3,15 @@ import { CUSTOMER_ROLE } from './constants';
 export class User {
   userId?: string = '';
   username?: string = '';
+  firstName?: string = '';
+  lastname?: string = '';
   email: string = '';
+  phoneNumber?: string = '000-000-0000';
+  addressLineOne?: string = '';
+  addressLineTwo?: string = '';
+  city?: string = '';
+  state?: string = '';
+  zip?: number = 0;
   password?: string = '';
   userRole?: string = CUSTOMER_ROLE;
 }
