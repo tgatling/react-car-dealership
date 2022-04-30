6fb6b42d6def9955899a18fd48c8c0eb0d82f54b
tgatling <tequesha.l.gatling@gmail.com>
2022-04-30 17:16:01 -0400

    styling for bill items and button to view payments for each bill

--- src/components/bills/BillItem.tsx
+++ src/components/bills/BillItem.tsx
@@ -14,40 +14,65 @@ const BillItem = ({ bill }: billProp) => {
   const { billId, billNumber, paymentDueDate, amountDue, paymentCompleted } =
     bill;
 
+  let billTitle =
+    bill.billNumber === 0 ? 'DOWN PAYMENT' : `BILL NUMBER ${billNumber}`;
+
   return (
     <div className={styles.billContainer}>
-      <div className={styles.billElement}>
-          <div className={styles.billIcon}>
-            <div
-              className={
-                bill.paymentCompleted ? styles.checkmark : styles.checkbox
-              }
-            >
-              <img
-                src={bill.paymentCompleted ? checkMark : checkbox}
-                alt='bill'
-              />
-            </div>
+      <div className={styles.billLeftContainer}>
+        <div className={styles.billIcon}>
+          <div
+            className={
+              bill.paymentCompleted ? styles.checkmark : styles.checkbox
+            }
+          >
+            <img
+              src={bill.paymentCompleted ? checkMark : checkbox}
+              alt='bill'
+            />
           </div>
-        <div className={styles.billHeader}>
-          <h2>Bill Title</h2>
         </div>
-      </div>
-      <div className={styles.billElement}>
         <div className={styles.billStatus}>
           {paymentCompleted && <p>PAID</p>}
           {!paymentCompleted && <button>PAY NOW</button>}
         </div>
-        <div className={styles.billDetails}>
-          <div className={styles.billRow}>
-            <div className={styles.billRowElements}>
-              <div className={styles.row}>
-                <label>{'Bill Id: '}</label>
-                <p>{bill.billId}</p>
+      </div>
+      <div className={styles.billRightContainer}>
+        <div className={styles.billHeader}>
+          <h2>{billTitle}</h2>
+        </div>
+        <div className={styles.billElement}>
+          <div className={styles.billDetails}>
+            <div className={styles.billRow}>
+              <div className={styles.billRowElements}>
+                <div className={styles.row}>
+                  <label>{'Due Date: '}</label>
+                  <p>{`00/00/0000`}</p>
+                </div>
+              </div>
+              <div className={styles.billRowElements}>
+                <div className={styles.row}>
+                  <label>{'Amount Due: '}</label>
+                  <p>{`$${bill.amountDue}`}</p>
+                </div>
               </div>
             </div>
-            <div className={styles.billRowElements}>
-              <p>Text</p>
+          </div>
+        </div>
+        <div className={styles.billElement}>
+          <div className={styles.billDetails}>
+            <div className={styles.billRow}>
+              <div className={styles.billRowElements}>
+                <div className={styles.row}>
+                  <label>{'Number of Bill Payments Made: '}</label>
+                  <p>{bill.paymentIds.length}</p>
+                </div>
+              </div>
+              <div className={styles.billRowElements}>
+                <div className={styles.viewPaymentsButton}>
+                  <button>VIEW PAYMENTS</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
