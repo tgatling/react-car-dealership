0ced6738bce5860a9fa9f3b5415d8448afda6312
tgatling <tequesha.l.gatling@gmail.com>
2022-07-29 16:40:38 -0400

    editing table for displaying bill, working on column headers

--- src/components/bills/BillBreakdown.tsx
+++ src/components/bills/BillBreakdown.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 
-import styles from './BillBreakdown.module.css'
+import styles from './BillBreakdown.module.css';
 
 const BillBreakdown = () => {
-    return (
-        <div className={styles.table}>
-            <div className={styles.row}>
-                <div>
-                    Bill
-                </div>
-                <div>
-                    Payment
-                </div>
-            </div>
-            <div>
-                Row 1
-            </div>
+  return (
+    <div className={styles.table}>
+      <div className={styles.row}>
+        <div className={styles.columnBar}>
+          <div className={styles.columnHeader}>Bill #</div>
+          <div className={styles.columnHeader}>Payment #</div>
         </div>
-    );
+      </div>
+      <div>Row 1</div>
+    </div>
+  );
 };
 
 export default BillBreakdown;
